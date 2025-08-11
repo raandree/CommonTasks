f566b89d0a1a5e406a7ccff1c56924981c5abb78
Raimund Andree <r.andree@live.com>
2025-08-11 23:22:10 +0200

    Update for Debugging

--- tests/DebugSingleResource.ps1
+++ tests/DebugSingleResource.ps1
@@ -1,23 +1,30 @@
 Import-Module -Name datum
 $here = $PSScriptRoot
 
+if ($PSVersionTable.PSEdition -eq 'Desktop')
+{
+    Import-Module -Name PSDesiredStateConfiguration -RequiredVersion 1.1
+}
+
 $datum = New-DatumStructure -DefinitionFile $here\Unit\DSCResources\Assets\Datum.yml
 $allNodes = Get-Content -Path $here\Unit\DSCResources\Assets\AllNodes.yml -Raw | ConvertFrom-Yaml
 
+$previousPSModulePath = $env:PSModulePath
 Write-Host 'Reading DSC Resource metadata for supporting CIM based DSC parameters...'
 Initialize-DscResourceMetaInfo -ModulePath $here\..\output\RequiredModules\
 Write-Host 'Done'
+$env:PSModulePath = $previousPSModulePath
 
 $global:configurationData = @{
     AllNodes = [array]$allNodes
     Datum    = $Datum
 }
 
-$dscResourceName = 'HyperV'
+$dscResourceName = 'DnsServerRootHints'
 
 configuration "Config_$dscResourceName" {
 
-    Import-DscResource -ModuleName CommonTasks -Name HyperV
+    Import-DscResource -ModuleName CommonTasks -Name DnsServerRootHints
 
     node "localhost_$dscResourceName" {
 
